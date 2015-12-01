bb53638b40d81c7e3a81cc98b57dc95fe18ad59f
Harry Rickards <rickards@mit.edu>
2015-12-01 05:05:41 +0000

    Ignore non-confession posts when scraping

--- parser/parse.py
+++ parser/parse.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from nltk.parse.bllip import BllipParser
 from bllipparser.ModelFetcher import download_and_install_model
 from nltk import word_tokenize, sent_tokenize
@@ -5,7 +6,7 @@ from pymongo import MongoClient
 from progressbar import ProgressBar
 
 # version parsing logic so we don't unnecessarily reparse confessions
-PARSER_VERSION = 1
+PARSER_VERSION = 2
 
 # download SANCL2012-Uniform (trained on WSJ Portion of OntoNotes
 # and Google Web Treebank) model if not already present
@@ -18,16 +19,19 @@ bllip = BllipParser.from_unified_model_dir(model_dir)
 # db
 client = MongoClient()
 db = client.confessions
-db.parses.drop()
+# db.parses.drop()
 
 # find all raw confessions not already parsed (by this version of the parser)
 confessions = db.confessions.find({
     "parsed": { "$ne": PARSER_VERSION }
-})
+}, limit=1000)
 total = confessions.count()
 current = 0
 with ProgressBar(max_value=total) as progress:
     for confession in confessions:
+        # skip datums that aren't confessions (updates/photos/etc from FB page)
+        if not ("message" in confession): continue
+
         # tokenize confession post into sentences using pretained NLTK punkt tokenizer
         sentences = sent_tokenize(confession["message"])
         trees = [] # one tree per sentence
