42fbd18773eae017986e00750ee8e9f69a8ce58f
Harry Rickards <rickards@mit.edu>
2015-12-12 20:08:45 +0000

    Accept both trees and confessions into treelstm wrapper

--- sentiment/predict.py
+++ sentiment/predict.py
@@ -5,59 +5,61 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.tree import ParentedTree, Tree
 from pymongo import MongoClient
 from progressbar import ProgressBar
+import numpy as np
 import subprocess
 
 class SentimentPredictor():
     def __init__(self):
-        # line number counter
-        self.current_sentence = 0
         # confessions processed
         self.confessions = []
+        self.trees = []
         self.predictions = []
 
         # files to write output
         self.parents_file = open("parents.txt", "wb")
         self.sents_file = open("sents.txt", "wb")
 
+    def add_tree(self, datum):
+        # parse tree and binarize
+        tree = Tree.fromstring(datum["raw_tree"])
+        tree.chomsky_normal_form()
+        tree.collapse_unary(collapsePOS=True)
+        tree = ParentedTree.convert(tree)
 
-    def add(self, confession):
+        # assign indices to subtrees
+        indices = {}
+        counter = 0
+        for t in tree.subtrees():
+            indices[t.treeposition()] = counter
+            counter += 1
+
+        # generate parent pointers and labels
+        # (labels = one instance of sent in sents by treelstm terminology)
+        parents = [0] * (counter - 1)
+        labels = []
+        counter = 0
+        for t in tree.subtrees():
+            parent = t.parent()
+            if parent != None:
+                parents[counter] = indices[parent.treeposition()]
+                counter += 1
+            if type(t[0]) is str or type(t[0]) is unicode: labels.append(t[0])
+
+        self.parents_file.write(" ".join(map(str, parents)) + "\n")
+        self.sents_file.write(" ".join(labels) + "\n")
+        self.trees.append(datum)
+        return len(self.trees) - 1 # ID
+
+    def add_confession(self, confession):
         # store line numbers
-        confession["sentences"] = []
+        confession["tree_ids"] = []
 
         # analyze each sentence seperately
         for raw_tree in confession["trees"]:
             if raw_tree == "None": continue
-            # parse tree and binarize
-            tree = Tree.fromstring(raw_tree)
-            tree.chomsky_normal_form()
-            tree.collapse_unary(collapsePOS=True)
-            tree = ParentedTree.convert(tree)
-
-            # assign indices to subtrees
-            indices = {}
-            counter = 0
-            for t in tree.subtrees():
-                indices[t.treeposition()] = counter
-                counter += 1
-
-            # generate parent pointers and labels
-            # (labels = one instance of sent in sents by treelstm terminology)
-            parents = [0] * (counter - 1)
-            labels = []
-            counter = 0
-            for t in tree.subtrees():
-                parent = t.parent()
-                if parent != None:
-                    parents[counter] = indices[parent.treeposition()]
-                    counter += 1
-                if type(t[0]) is str or type(t[0]) is unicode: labels.append(t[0])
-
-            self.parents_file.write(" ".join(map(str, parents)) + "\n")
-            self.sents_file.write(" ".join(labels) + "\n")
-
-            # store and increment sentence ID
-            confession["sentences"].append(self.current_sentence)
-            self.current_sentence += 1
+            confession["tree_ids"].append(self.add_tree({
+                "raw_tree": raw_tree
+            }))
 
         # store confession
         self.confessions.append(confession)
@@ -72,11 +74,21 @@ class SentimentPredictor():
 
         # open predictions, and assign to confessions
         with open("predictions.txt", "rb") as predictions_file:
-            # 3 = positive, 1 = negative
-            # ---> 1 = positive, -1 = negative
-            self.predictions = [float(line.strip()) - 2 for line in predictions_file.readlines()]
+            # 1 = negative prob, 2 = neutral prob, 3 = positive prob
+            self.predictions = []
+            self.expected_values = []
+            for line in predictions_file:
+                predictions = map(float, line.split(",")[:3])
+                # convert from log likelihood
+                predictions = map(np.exp, predictions)
+                self.predictions.append(predictions[2] - predictions[0])
+
+        for i, datum in enumerate(self.trees):
+            datum["sentiment"] = self.predictions[i]
+
         for i, confession in enumerate(self.confessions):
-            confession["sentiments"] = [self.predictions[sentence] for sentence in confession["sentences"]]
+            confession["sentiments"] = [self.predictions[tree_id] for tree_id in confession["tree_ids"]]
+            confession["sentiment"] = sum(confession["sentiments"])*len(confession["sentiments"])
             self.confessions[i] = confession
 
 if __name__ == "__main__":
@@ -85,14 +97,14 @@ if __name__ == "__main__":
     db = client.confessions
 
     # find all parsed confessions
-    parses = db.parses.find(limit=100)
+    parses = db.parses.find(limit=1)
     total = parses.count()
     current_confession = 0
     predictor = SentimentPredictor()
     with ProgressBar(max_value=total + 1) as progress:
         for confession in parses:
             # preprocess
-            predictor.add(confession)
+            predictor.add_confession(confession)
 
             # update progress bar
             # progress.update(current_confession)
@@ -103,7 +115,5 @@ if __name__ == "__main__":
 
     # print output
     for confession in predictor.confessions:
-        for tree, prediction in zip(confession["trees"], confession["sentiments"]):
-            print " ".join(Tree.fromstring(tree).leaves())
-            print prediction
-            print
+        print confession["message"]
+        print confession["sentiment"]
